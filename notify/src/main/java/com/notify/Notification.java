767173f26ca57ae51209128cefec56b8417e8a20
Sagar M Bandennavar <53654068+SagaMB@users.noreply.github.com>
2023-07-24 10:50:51 +0530

    Add files via upload

---
+++ notify/src/main/java/com/notify/Notification.java
@@ -0,0 +1,11 @@
+package com.notify;
+
+import lombok.Data;
+
+@Data
+public class Notification {
+	
+	private String Name;
+	private Long Amount;
+
+}
---
+++ notify/src/main/java/com/notify/NotificationService.java
@@ -0,0 +1,11 @@
+package com.notify;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+	
+	public Notification saveNotification(Notification noti) {
+		return noti;
+	}
+}
